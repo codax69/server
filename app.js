60cd922a2121babd8b0e4d7c25e5d5000653f1e7
Pritesh Patel <pritbusiness03@gmail.com>
2024-09-24 21:38:55 +0530

    credentials true

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ import cookieParser from "cookie-parser";
 const app = express();
 app.use(json({ limit: "16kb" }));
 app.use(cookieParser());
-app.use(cors({ origin: "*" }));
+app.use(cors({ origin: "*",
+    credentials:true
+ }));
 app.use(urlencoded({ limit: "20kb", extended: true }));
 app.use(express.static("public"));
 
